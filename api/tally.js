0aa1880f8d1eb3a25c18f37059a192a655b1e819
Mowery79 <njmowery@gmail.com>
2026-02-22 16:36:03 -0700

    Delete api directory

--- api/tally.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
-  console.log("Tally payload:", JSON.stringify(req.body));
-  return res.status(200).json({ ok: true });
-}
